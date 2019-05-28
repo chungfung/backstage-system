62cf26c430f767a0039bf1878f8d0ba33b9b1f0f
chungfung <fy_java@163.com>
2019-05-28 16:15:07 +0800

    基于开源框架dwz和xxl-job的后台管理系统模板

--- system-framework/src/main/java/com/system/framework/web/exception/GlobalExceptionHandler.java
+++ system-framework/src/main/java/com/system/framework/web/exception/GlobalExceptionHandler.java
@@ -1,95 +1,37 @@
 package com.system.framework.web.exception;
 
-import com.system.common.core.domain.AjaxResult;
-import com.system.common.exception.BusinessException;
-import com.system.common.exception.DemoModeException;
-import com.system.common.utils.ServletUtils;
-import com.system.common.utils.security.PermissionUtils;
-import org.apache.shiro.authz.AuthorizationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 全局异常处理器
  * 
  * @author system
  */
-@RestControllerAdvice
-public class GlobalExceptionHandler
-{
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    /**
-     * 权限校验失败 如果请求为ajax返回json，普通请求跳转页面
-     */
-    @ExceptionHandler(AuthorizationException.class)
-    public Object handleAuthorizationException(HttpServletRequest request, AuthorizationException e)
-    {
-        log.error(e.getMessage(), e);
-        if (ServletUtils.isAjaxRequest(request))
-        {
-            return AjaxResult.error(PermissionUtils.getMsg(e.getMessage()));
+    @ExceptionHandler(value = Exception.class)
+    public Object errorHandler(Exception ex,HttpServletRequest req) {
+        if(req.getHeader("X-Requested-With")!=null
+                && "XMLHttpRequest".equals(req.getHeader("X-Requested-With").toString())) {
+            Map<String,Object> map = new HashMap<String,Object>();
+            map.put("code", 500);
+            map.put("msg", ex.getMessage());
+            return map;
+        } else {
+            ModelAndView mav = new ModelAndView();
+            mav.setViewName("error/index");
+            return mav;
         }
-        else
-        {
-            ModelAndView modelAndView = new ModelAndView();
-            modelAndView.setViewName("error/unauth");
-            return modelAndView;
-        }
-    }
-
-    /**
-     * 请求方式不支持
-     */
-    @ExceptionHandler({ HttpRequestMethodNotSupportedException.class })
-    public AjaxResult handleException(HttpRequestMethodNotSupportedException e)
-    {
-        log.error(e.getMessage(), e);
-        return AjaxResult.error("不支持' " + e.getMethod() + "'请求");
-    }
-
-    /**
-     * 拦截未知的运行时异常
-     */
-    @ExceptionHandler(RuntimeException.class)
-    public AjaxResult notFount(RuntimeException e)
-    {
-        log.error("运行时异常:", e);
-        return AjaxResult.error("运行时异常:" + e.getMessage());
-    }
-
-    /**
-     * 系统异常
-     */
-    @ExceptionHandler(Exception.class)
-    public AjaxResult handleException(Exception e)
-    {
-        log.error(e.getMessage(), e);
-        return AjaxResult.error("服务器错误，请联系管理员");
-    }
-
-    /**
-     * 业务异常
-     */
-    @ExceptionHandler(BusinessException.class)
-    public AjaxResult businessException(BusinessException e)
-    {
-        log.error(e.getMessage(), e);
-        return AjaxResult.error(e.getMessage());
-    }
-
-    /**
-     * 演示模式异常
-     */
-    @ExceptionHandler(DemoModeException.class)
-    public AjaxResult demoModeException(DemoModeException e)
-    {
-        return AjaxResult.error("演示模式，不允许操作");
     }
 }
