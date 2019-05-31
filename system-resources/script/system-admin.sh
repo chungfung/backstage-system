a2ef5b8943c99b62f2df6616e12922d7f7f49158
chungfung <fy_java@163.com>
2019-05-31 16:04:58 +0800

    基于dwz和xxl-job的后台管理系统模板

--- system-resources/script/system-admin.sh
+++ system-resources/script/system-admin.sh
@@ -1,21 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
 AppName=system-admin.jar
 
 #JVM参数
 JVM_OPTS="-Dname=$AppName  -Duser.timezone=Asia/Shanghai -Xms512M -Xmx512M -XX:PermSize=256M -XX:MaxPermSize=512M -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
-APP_HOME=`pwd`
-LOG_PATH=$APP_HOME/logs/$AppName.log
 
 if [ "$1" = "" ];
 then
-    echo -e "\033[0;31m 未输入操作名 \033[0m  \033[0;34m {start|stop|restart|status} \033[0m"
+    echo -e "\033[0;31m 请输入操作名： \033[0m  \033[0;34m {start|stop|restart|status} \033[0m"
     exit 1
 fi
 
 if [ "$AppName" = "" ];
 then
-    echo -e "\033[0;31m 未输入应用名 \033[0m"
+    echo -e "\033[0;31m 请输入应用名 \033[0m"
     exit 1
 fi
 
@@ -24,17 +22,28 @@ function start()
     PID=`ps -ef |grep java|grep $AppName|grep -v grep|awk '{print $2}'`
 
 	if [ x"$PID" != x"" ]; then
-	    echo "$AppName is running..."
+	    echo "$AppName 正在运行..."
 	else
-		nohup java -jar  $JVM_OPTS target/$AppName > /dev/null 2>&1 &
-		echo "Start $AppName success..."
+		nohup java -jar  $JVM_OPTS $AppName > /dev/null 2>&1 &
+		echo "$AppName 启动成功..."
+		echo " .----------------.  .----------------.  .----------------.  .----------------.
+| .--------------. || .--------------. || .--------------. || .--------------. |
+| |  ____  ____  | || |  _________   | || |      __      | || |  ____  ____  | |
+| | |_  _||_  _| | || | |_   ___  |  | || |     /  \     | || | |_   ||   _| | |
+| |   \ \  / /   | || |   | |_  \_|  | || |    / /\ \    | || |   | |__| |   | |
+| |    \ \/ /    | || |   |  _|  _   | || |   / ____ \   | || |   |  __  |   | |
+| |    _|  |_    | || |  _| |___/ |  | || | _/ /    \ \_ | || |  _| |  | |_  | |
+| |   |______|   | || | |_________|  | || ||____|  |____|| || | |____||____| | |
+| |              | || |              | || |              | || |              | |
+| '--------------' || '--------------' || '--------------' || '--------------' |
+ '----------------'  '----------------'  '----------------'  '----------------' "
 	fi
 }
 
 function stop()
 {
     echo "Stop $AppName"
-	
+
 	PID=""
 	query(){
 		PID=`ps -ef |grep java|grep $AppName|grep -v grep|awk '{print $2}'`
@@ -43,15 +52,15 @@ function stop()
 	query
 	if [ x"$PID" != x"" ]; then
 		kill -TERM $PID
-		echo "$AppName (pid:$PID) exiting..."
+		echo "$AppName (pid:$PID) 已停止..."
 		while [ x"$PID" != x"" ]
 		do
 			sleep 1
 			query
 		done
-		echo "$AppName exited."
+		echo "$AppName 已停止."
 	else
-		echo "$AppName already stopped."
+		echo "$AppName 已经停止."
 	fi
 }
 
@@ -66,9 +75,9 @@ function status()
 {
     PID=`ps -ef |grep java|grep $AppName|grep -v grep|wc -l`
     if [ $PID != 0 ];then
-        echo "$AppName is running..."
+        echo "$AppName 正在运行..."
     else
-        echo "$AppName is not running..."
+        echo "$AppName 已停止..."
     fi
 }
 
