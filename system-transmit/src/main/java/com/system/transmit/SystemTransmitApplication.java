f1d1f845fef7706f1867389eb62c7e4e48d510ff
chungfung <fy_java@163.com>
2019-06-04 17:12:31 +0800

    基于dwz和xxl-job的后台管理系统模板

---
+++ system-transmit/src/main/java/com/system/transmit/SystemTransmitApplication.java
@@ -0,0 +1,13 @@
+package com.system.transmit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemTransmitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemTransmitApplication.class, args);
+	}
+
+}
