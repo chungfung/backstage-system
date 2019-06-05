8686bbe27929b6650143dfe683aaee054f5687f9
chungfung <fy_java@163.com>
2019-06-05 15:37:32 +0800

    基于dwz和xxl-job的后台管理系统模板

--- system-admin/src/main/java/com/system/SystemAdminApplication.java
+++ system-admin/src/main/java/com/system/SystemAdminApplication.java
@@ -1,11 +1,13 @@
 package com.system;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.system.service.mapper")
+@EncryptablePropertySource(name = "spring.datasource.password", value = "classpath:application-druid.yml")
 public class SystemAdminApplication {
 
 	public static void main(String[] args) {
